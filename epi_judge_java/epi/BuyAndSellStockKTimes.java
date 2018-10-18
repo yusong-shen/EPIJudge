054f99268f20f56cc981e083316a6d19aa07c712
yusong-shen <yusongtju@gmail.com>
2018-10-17 23:36:17 -0500

    24.3 - buy stock k times

--- epi_judge_java/epi/BuyAndSellStockKTimes.java
+++ epi_judge_java/epi/BuyAndSellStockKTimes.java
@@ -1,13 +1,36 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Arrays;
 import java.util.List;
 public class BuyAndSellStockKTimes {
   @EpiTest(testDataFile = "buy_and_sell_stock_k_times.tsv")
 
   public static double buyAndSellStockKTimes(List<Double> prices, int k) {
-    // TODO - you fill in here.
-    return 0.0;
+    if (k > prices.size() / 2) return buyAndSellStockUnlimited(prices);
+    double[] buy = new double[k];
+    double[] sell = new double[k + 1];
+    Arrays.fill(buy, Integer.MIN_VALUE);
+    Arrays.fill(sell, Integer.MIN_VALUE);
+    sell[0] = 0.0;
+    sell[k] = 0.0;
+    for (double p : prices) {
+      for (int i = 0; i < k; i++) {
+        buy[i] = Math.max(buy[i], sell[i] - p);
+        sell[i + 1] = Math.max(sell[i + 1], buy[i] + p);
+      }
+    }
+    return sell[k];
+  }
+
+  public static double buyAndSellStockUnlimited(List<Double> prices) {
+    double maxDiff = 0.0;
+    for (int i = 1; i < prices.size(); i++) {
+      maxDiff += Math.max(0, prices.get(i) - prices.get(i - 1));
+    }
+
+    return maxDiff;
   }
 
   public static void main(String[] args) {
