9bd8cca4660320bfe96d63c94b0f6cf331c1ccce
yusong-shen <yusongtju@gmail.com>
2018-10-21 12:05:52 -0500

    6.8(5.9) - Prime Sieve by exclude all prime's multiple

--- epi_judge_java/epi/PrimeSieve.java
+++ epi_judge_java/epi/PrimeSieve.java
@@ -1,13 +1,32 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 public class PrimeSieve {
   @EpiTest(testDataFile = "prime_sieve.tsv")
   // Given n, return all primes up to and including n.
   public static List<Integer> generatePrimes(int n) {
-    // TODO - you fill in here.
-    return null;
+    boolean[] isPrime = new boolean[n + 1];
+    Arrays.fill(isPrime, true);
+    List<Integer> results = new ArrayList<>();
+    isPrime[1] = false;
+    for (int i = 2; i <= n; i++) {
+      if (isPrime[i]) {
+        // exclude all its multiples
+        int k = i * 2;
+        while (k <= n) {
+          isPrime[k] = false;
+          k += i;
+        }
+        results.add(i);
+      }
+    }
+
+    return results;
   }
 
   public static void main(String[] args) {
