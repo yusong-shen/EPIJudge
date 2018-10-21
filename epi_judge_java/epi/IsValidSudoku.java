777884ecf6e72ff7b168178eaf560aa5b4f5de25
yusong-shen <yusongtju@gmail.com>
2018-10-20 22:04:30 -0500

    6.16 (5.17) - The Sudoku Checker

--- epi_judge_java/epi/IsValidSudoku.java
+++ epi_judge_java/epi/IsValidSudoku.java
@@ -7,7 +7,42 @@ public class IsValidSudoku {
 
   // Check if a partially filled matrix has any conflicts.
   public static boolean isValidSudoku(List<List<Integer>> partialAssignment) {
-    // TODO - you fill in here.
+    boolean[] marked = new boolean[10];
+    // rows
+    for (List<Integer> row : partialAssignment) {
+      marked = new boolean[10];
+      for (int v : row) {
+        if (v == 0) continue;
+        if (marked[v]) return false;
+        marked[v] = true;
+      }
+    }
+    // cols
+    for (int j = 0; j < 9; j++) {
+      marked = new boolean[10];
+      for (int i = 0; i < 9; i++) {
+        int v = partialAssignment.get(i).get(j);
+        if (v == 0) continue;
+        if (marked[v]) return false;
+        marked[v] = true;
+      }
+    }
+
+    // 9 * 3x3 submatrix
+    for (int k = 0; k < 9; k++) {
+      int x = (k % 3) * 3;
+      int y = (k / 3) * 3;
+      marked = new boolean[10];
+      for (int i = x; i < x + 3; i++) {
+        for (int j = y; j < y + 3; j++) {
+          int v = partialAssignment.get(i).get(j);
+          if (v == 0) continue;
+          if (marked[v]) return false;
+          marked[v] = true;
+        }
+      }
+    }
+
     return true;
   }
 
