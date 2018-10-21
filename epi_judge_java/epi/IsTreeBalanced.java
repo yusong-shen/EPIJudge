505560407cc0546fe2eae4d421e60b0e1f22df5d
yusong-shen <yusongtju@gmail.com>
2018-10-21 17:04:17 -0500

    10.1(9.1) - Test if a tree is balanced

--- epi_judge_java/epi/IsTreeBalanced.java
+++ epi_judge_java/epi/IsTreeBalanced.java
@@ -5,9 +5,23 @@ public class IsTreeBalanced {
 
   @EpiTest(testDataFile = "is_tree_balanced.tsv")
 
+  /**
+   * A binary tree is said to be height-balanced if for each node in the tree, the difference in the height of its left
+   * and right subtrees is at most one.
+   */
   public static boolean isBalanced(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return getDepth(tree) >= 0;
+  }
+
+  private static int getDepth(BinaryTreeNode<Integer> node) {
+    if (node == null) {
+      return  0;
+    }
+    int ld = getDepth(node.left);
+    int rd = getDepth(node.right);
+    boolean isBalanced = (ld >= 0) && (rd >= 0) && (Math.abs(ld - rd) <= 1);
+    int depth = Math.max(Math.abs(ld), Math.abs(rd)) + 1;
+    return isBalanced ? depth : -depth;
   }
 
   public static void main(String[] args) {
