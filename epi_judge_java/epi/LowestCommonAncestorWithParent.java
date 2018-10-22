086544a2c5714555e603a2623168bbb266bb878c
yusong-shen <yusongtju@gmail.com>
2018-10-21 20:19:24 -0500

    10.3(9.4) - Compute the LCA when nodes have parent pointers

--- epi_judge_java/epi/LowestCommonAncestorWithParent.java
+++ epi_judge_java/epi/LowestCommonAncestorWithParent.java
@@ -8,9 +8,36 @@ public class LowestCommonAncestorWithParent {
 
   public static BinaryTree<Integer> LCA(BinaryTree<Integer> node0,
                                         BinaryTree<Integer> node1) {
-    // TODO - you fill in here.
-    return null;
+    int d0 = getDepth(node0);
+    int d1 = getDepth(node1);
+    // move node0 and node1 to the same level
+    if (d0 > d1) {
+      for (int i = 0; i < d0 - d1; i++) {
+        node0 = node0.parent;
+      }
+    } else {
+      for (int i = 0; i < d1 - d0; i++) {
+        node1 = node1.parent;
+      }
+    }
+    BinaryTree<Integer> lca = null;
+    while (node0 != node1) {
+      node0 = node0.parent;
+      node1 = node1.parent;
+    }
+    lca = node0;
+    return lca;
   }
+
+  private static int getDepth(BinaryTree<Integer> node0) {
+    int depth = 0;
+    while (node0 != null) {
+      node0 = node0.parent;
+      depth++;
+    }
+    return depth;
+  }
+
   @EpiTest(testDataFile = "lowest_common_ancestor.tsv")
   public static int lcaWrapper(TimedExecutor executor, BinaryTree<Integer> tree,
                                Integer key0, Integer key1) throws Exception {
