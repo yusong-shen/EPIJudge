8f43b57777659c9924a9ac2c360cf9aa834ee458
yusong-shen <yusongtju@gmail.com>
2018-10-23 23:57:22 -0500

    9.11 - iterative inorder O(logn) space solution

--- epi_judge_java/epi/TreeInorder.java
+++ epi_judge_java/epi/TreeInorder.java
@@ -1,13 +1,34 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
+
 public class TreeInorder {
   @EpiTest(testDataFile = "tree_inorder.tsv")
 
   public static List<Integer> inorderTraversal(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    Stack<BinaryTreeNode<Integer>> stack = new Stack<>();
+    List<Integer> result = new ArrayList<>();
+    if (tree == null) return  result;
+    BinaryTreeNode<Integer> cur = tree;
+    while (cur != null) {
+      stack.push(cur);
+      cur = cur.left;
+    }
+    while (!stack.isEmpty()) {
+      cur = stack.pop();
+      result.add(cur.data);
+      cur = cur.right;
+      while (cur != null) {
+        stack.push(cur);
+        cur = cur.left;
+      }
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
