5bdfc8b228eaa6bdc4c13fbf7f43d842d4f3e8ab
yusong-shen <yusongtju@gmail.com>
2018-10-24 23:19:11 -0500

    15.1 - basic hanoi tower

--- epi_judge_java/epi/Hanoi.java
+++ epi_judge_java/epi/Hanoi.java
@@ -13,9 +13,33 @@ public class Hanoi {
   private static final int NUM_PEGS = 3;
 
   public static List<List<Integer>> computeTowerHanoi(int numRings) {
-    // TODO - you fill in here.
-    return Collections.emptyList();
+    List<Deque<Integer>> pegs = new ArrayList<>();
+    for (int i = 0; i < NUM_PEGS; i++) {
+      pegs.add(new LinkedList<>());
+    }
+    // Initialize pegs.
+    for (int i = numRings; i >= 1; --i) {
+      pegs.get(0).addFirst(i);
+    }
+    List<List<Integer>> result = new ArrayList<>();
+    computeTowerHanoiSteps(numRings, pegs, 0, 1, 2, result);
+    return result;
   }
+
+  private static void computeTowerHanoiSteps(int numRingsToMove,
+                                             List<Deque<Integer>> pegs,
+                                             int fromPeg, int toPeg, int usePeg,
+                                             List<List<Integer>> result) {
+    if (numRingsToMove > 0) {
+      computeTowerHanoiSteps(numRingsToMove - 1, pegs, fromPeg, usePeg, toPeg,
+              result);
+      pegs.get(toPeg).addFirst(pegs.get(fromPeg).removeFirst());
+      result.add(List.of(fromPeg, toPeg));
+      computeTowerHanoiSteps(numRingsToMove - 1, pegs, usePeg, toPeg, fromPeg,
+              result);
+    }
+  }
+
   @EpiTest(testDataFile = "hanoi.tsv")
   public static void computeTowerHanoiWrapper(TimedExecutor executor,
                                               int numRings) throws Exception {
