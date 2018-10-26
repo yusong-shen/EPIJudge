270e178e759be5f284a737c84367cebb47a12b5b
yusong-shen <yusongtju@gmail.com>
2018-10-25 22:47:50 -0500

    10.13 - RECONSTRUCTA BINARY TREE FROM A PREORDER TRAVERSAL WITH MARKERS ,incorrect implementation of iterative version

--- epi_judge_java/epi/TreeFromPreorderWithNull.java
+++ epi_judge_java/epi/TreeFromPreorderWithNull.java
@@ -2,13 +2,38 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import epi.test_framework.TimedExecutor;
-import java.util.ArrayList;
-import java.util.List;
+
+import java.util.*;
+
 public class TreeFromPreorderWithNull {
   public static BinaryTreeNode<Integer>
   reconstructPreorder(List<Integer> preorder) {
-    // TODO - you fill in here.
-    return null;
+    Set<BinaryTreeNode<Integer>> leftVisited = new HashSet<>();
+    Stack<BinaryTreeNode<Integer>> stack = new Stack<>();
+    for (Integer v : preorder) {
+      if (stack.empty()) {
+        stack.push(new BinaryTreeNode<>(v));
+      } else {
+        BinaryTreeNode<Integer> prev = stack.peek();
+        BinaryTreeNode<Integer> cur = (v == null) ? null : new BinaryTreeNode<>(v);
+        if (!leftVisited.contains(prev) && prev.left == null) {
+          prev.left = cur;
+          leftVisited.add(prev);
+        } else if (leftVisited.contains(prev) && prev.right == null){
+          leftVisited.remove(prev);
+          prev.right = cur;
+          stack.pop();
+        }
+        if (cur != null) {
+          stack.push(cur);
+        }
+      }
+    }
+    BinaryTreeNode<Integer> root = null;
+    while (!stack.isEmpty()) {
+      root = stack.pop();
+    }
+    return root;
   }
   @EpiTest(testDataFile = "tree_from_preorder_with_null.tsv")
   public static BinaryTreeNode<Integer>
