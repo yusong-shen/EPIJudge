0e633cf3ad5151c767535a9a78457c3b116b4404
yusong-shen <yusongtju@gmail.com>
2018-10-27 16:27:52 -0500

    16.2 - all placement for queens (need to be further reviewed)

--- epi_judge_java/epi/NQueens.java
+++ epi_judge_java/epi/NQueens.java
@@ -3,6 +3,8 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiPredicate;
 public class NQueens {
@@ -10,8 +12,46 @@ public class NQueens {
 
   public static List<List<Integer>> nQueens(int n) {
     // TODO - you fill in here.
-    return null;
+    List<List<Integer>> result = new ArrayList<>();
+    List<Integer> colPlacement = new ArrayList<>();
+    solve(n, 0, colPlacement, result);
+    return result;
   }
+
+  /**
+   * Try each row
+   * @param n
+   * @param rows number of rows that legally placed
+   * @param colPlacement ith element in colPlacement : column number that queen placed in ith row
+   * @param ret
+   */
+  private static void solve(int n, int rows, List<Integer> colPlacement, List<List<Integer>> ret) {
+    if (rows == n) {
+      // need to make a copy of colPlacement
+      ret.add(new ArrayList<>(colPlacement));
+      return;
+    }
+    for (int col = 0; col < n; col++) {
+      colPlacement.add(col);
+      if (isValid(colPlacement)) {
+        solve(n, rows + 1, colPlacement, ret);
+      }
+      // backtrack
+      colPlacement.remove(colPlacement.size() - 1);
+    }
+
+  }
+
+  // test if a newly placed queen conflicted with previous queens
+  private static boolean isValid(List<Integer> cols) {
+    int newRowId = cols.size() - 1;
+    for (int i = 0; i < cols.size() - 1; i++) {
+      int diff = Math.abs(cols.get(i) - cols.get(newRowId));
+      if (diff == 0 || diff == newRowId - i) return false;
+    }
+    return true;
+  }
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
