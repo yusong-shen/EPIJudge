a8eef59593ea4da5db81978b5c569823b38f4032
yusong-shen <yusongtju@gmail.com>
2018-10-27 17:41:44 -0500

    16.3 - generate permutations

--- epi_judge_java/epi/Permutations.java
+++ epi_judge_java/epi/Permutations.java
@@ -3,6 +3,8 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -10,9 +12,32 @@ public class Permutations {
   @EpiTest(testDataFile = "permutations.tsv")
 
   public static List<List<Integer>> permutations(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+    return permutations(A, A.size());
   }
+
+  private static List<List<Integer>> permutations(List<Integer> A, int len) {
+    List<List<Integer>> result = new ArrayList<>();
+    if (len == 0) {
+      result.add(new ArrayList<>());
+      return result;
+    }
+    if (len == 1) {
+      result.add(new ArrayList<>(A.subList(0, 1)));
+      return result;
+    }
+    List<List<Integer>> prevRet = permutations(A, len - 1);
+    int v = A.get(len - 1);
+    for (List<Integer> p : prevRet) {
+      for (int i = 0; i <= p.size(); i++) {
+        List<Integer> newP = new ArrayList<>(p);
+        newP.add(i, v);
+        result.add(newP);
+      }
+    }
+
+    return result;
+  }
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
