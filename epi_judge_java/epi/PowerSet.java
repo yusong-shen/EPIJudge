cdce81bf1e03184b706f387420d0325ca55c36e6
yusong-shen <yusongtju@gmail.com>
2018-10-28 12:53:59 -0500

    15.4 - power set without duplicate

--- epi_judge_java/epi/PowerSet.java
+++ epi_judge_java/epi/PowerSet.java
@@ -3,6 +3,8 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.EpiTestComparator;
 import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.function.BiPredicate;
@@ -10,9 +12,22 @@ public class PowerSet {
   @EpiTest(testDataFile = "power_set.tsv")
 
   public static List<List<Integer>> generatePowerSet(List<Integer> inputSet) {
-    // TODO - you fill in here.
-    return null;
+    List<Integer> curSet = new ArrayList<>();
+    List<List<Integer>> ret = new ArrayList<>();
+    powerSet(0, inputSet, curSet, ret);
+    return ret;
   }
+
+  private static void powerSet(int start, List<Integer> inputSet, List<Integer> curSet, List<List<Integer>> ret) {
+    ret.add(new ArrayList<>(curSet));
+    for (int i = start; i < inputSet.size(); i++) {
+      curSet.add(inputSet.get(i));
+      powerSet(i + 1, inputSet, curSet, ret);
+      curSet.remove(curSet.remove(curSet.size() - 1));
+    }
+
+  }
+
   @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
