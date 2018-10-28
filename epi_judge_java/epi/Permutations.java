62e74c7c9641c9fb0ea5ba8717fd06011aa8c14f
yusong-shen <yusongtju@gmail.com>
2018-10-28 13:13:34 -0500

    16.3 - permute with duplicates by swapping

--- epi_judge_java/epi/Permutations.java
+++ epi_judge_java/epi/Permutations.java
@@ -4,16 +4,15 @@ import epi.test_framework.EpiTestComparator;
 import epi.test_framework.LexicographicalListComparator;
 import epi.test_framework.GenericTest;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiPredicate;
 public class Permutations {
   @EpiTest(testDataFile = "permutations.tsv")
 
   public static List<List<Integer>> permutations(List<Integer> A) {
 //    return permutations(A, A.size());
-      return permuteUnique(A);
+//      return permuteUnique(A);
+      return permuteUniqueBySwap(A);
   }
 
   private static List<List<Integer>> permutations(List<Integer> A, int len) {
@@ -73,7 +72,59 @@ public class Permutations {
 
     }
 
-  @EpiTestComparator
+    public static List<List<Integer>> permuteUniqueBySwap(List<Integer> nums) {
+        List<List<Integer>> ret = new ArrayList<>();
+        List<Integer> permutation = new ArrayList<>();
+//        Collections.sort(nums);
+        backtrackSwap(0, nums, permutation, ret);
+        return ret;
+    }
+
+    /**
+     * Since we only need permutations of the array, the actual "content" does not change, we could find each
+     * permutation by swapping the elements in the array.
+     *
+     * The idea is for each recursion level, swap the current element at 1st index with each element that comes
+     * after it (including itself). For example, permute[1,2,3]:
+     *
+     * At recursion level 0, current element at 1st index is 1, there are 3 possibilities: [1] + permute[2,3],
+     * [2] + permute[1,3], [3] + permute[2,1].
+     *
+     * Take "2+permute[1,3]" as the example at recursion level 0. At recursion level 1, current elemenet at 1st
+     * index is 1, there are 2 possibilities: [2,1] + permute[3], [2,3] + permute[1].
+     *
+     * ... and so on.
+     *
+     * Let's look at another example, permute[1,2,3,4,1].
+     *
+     * At recursion level 0, we have [1] + permute[2,3,4,1], [2] + permute[1,3,4,1], [3] + permute[2,1,4,1],
+     * [4] + permute[2,3,1,1], [1] + permute[2,3,4,1].
+     *
+     * 1 has already been at the 1st index of current recursion level, so the last possibility is redundant.
+     * We can use a hash set to mark which elements have been at the 1st index of current recursion level, so
+     * that if we meet the element again, we can just skip it.
+     * @param start
+     * @param nums
+     * @param permutation
+     * @param ret
+     */
+    private static void backtrackSwap(int start, List<Integer> nums, List<Integer> permutation, List<List<Integer>> ret) {
+        if (permutation.size() == nums.size()) {
+            ret.add(new ArrayList<>(permutation));
+            return;
+        }
+        Set<Integer> used = new HashSet<>();
+        for (int i = start; i < nums.size(); i++) {
+            if (used.contains(nums.get(i))) continue;
+            used.add(nums.get(i));
+            Collections.swap(permutation, i, start);
+            backtrackSwap(start + 1, nums, permutation, ret);
+            Collections.swap(permutation, i, start);
+        }
+
+
+    }
+        @EpiTestComparator
   public static BiPredicate<List<List<Integer>>, List<List<Integer>>> comp =
       (expected, result) -> {
     if (result == null) {
