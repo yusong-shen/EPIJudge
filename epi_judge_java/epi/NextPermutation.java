dcc09d6bbc382a5b23a36f25a866ee4111b2bf56
yusong-shen <yusongtju@gmail.com>
2018-10-27 21:48:52 -0500

    6.10 - generate next permutation (need further review)

--- epi_judge_java/epi/NextPermutation.java
+++ epi_judge_java/epi/NextPermutation.java
@@ -1,13 +1,39 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 public class NextPermutation {
   @EpiTest(testDataFile = "next_permutation.tsv")
   public static List<Integer> nextPermutation(List<Integer> perm) {
-    // TODO - you fill in here.
-    return null;
+    // scan from right to left, find the first decreasing number
+    int i = perm.size() - 2;
+    while (i >= 0 && perm.get(i) >= perm.get(i + 1)) {
+      i--;
+    }
+    if (i == -1) return Collections.emptyList();
+
+    for (int j = perm.size() - 1; j > i; j--) {
+      if (perm.get(j) > perm.get(i)) {
+        Collections.swap(perm, j, i);
+        break;
+      }
+    }
+    reverse(perm, i + 1, perm.size() - 1);
+//    Collections.reverse(perm.subList(i + 1, perm.size()));
+    return perm;
   }
+
+  private static void reverse(List<Integer> perm, int start, int end) {
+    if (start >= end) return;
+    while (start < end) {
+      Collections.swap(perm, start++, end--);
+    }
+
+  }
+
 
   public static void main(String[] args) {
     System.exit(
