4d2572fc74dea97bfd54137c78a21e32d10f8bce
yusong-shen <yusongtju@gmail.com>
2018-11-04 17:00:23 -0600

    7.4 - replace and remove, O(n) space solution

--- epi_judge_java/epi/ReplaceAndRemove.java
+++ epi_judge_java/epi/ReplaceAndRemove.java
@@ -6,10 +6,35 @@ import java.util.ArrayList;
 import java.util.List;
 public class ReplaceAndRemove {
 
+  /**
+   * • Replace each 'a' by two 'd's.
+   * • Delete each entry containing a 'b'.
+   * [a,c,d,b,b,c,a] -> [d,d,c,d,c,d,d]
+   *
+   */
   public static int replaceAndRemove(int size, char[] s) {
-    // TODO - you fill in here.
-    return 0;
+    int nextLen = 0;
+    for (int i = 0; i < size; i++) {
+      char c = s[i];
+      if (c == 'a') nextLen += 2;
+      else if (c != 'b') nextLen++;
+    }
+    char[] result = new char[nextLen];
+    for (int i = 0, j = 0; i < size; i++) {
+      char c = s[i];
+      if (c == 'a') {
+        result[j++] = 'd';
+        result[j++] = 'd';
+      } else if (c != 'b') {
+        result[j++] = c;
+      }
+    }
+    for (int j = 0; j < result.length; j++) {
+      s[j] = result[j];
+    }
+    return nextLen;
   }
+
   @EpiTest(testDataFile = "replace_and_remove.tsv")
   public static List<String>
   replaceAndRemoveWrapper(TimedExecutor executor, Integer size, List<String> s)
