ebe591430f7da93e9c200f02f00a3bce17928106
yusong-shen <yusongtju@gmail.com>
2018-11-04 00:04:01 -0500

    24.27 - Skyline Lintcode Solution

--- epi_judge_java/epi/DrawingSkyline.java
+++ epi_judge_java/epi/DrawingSkyline.java
@@ -2,7 +2,9 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.EpiUserType;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class DrawingSkyline {
   @EpiUserType(ctorParams = {int.class, int.class, int.class})
 
@@ -45,8 +47,69 @@ public class DrawingSkyline {
 
   public static List<Rectangle> drawingSkylines(List<Rectangle> buildings) {
     // TODO - you fill in here.
-    return null;
+    List<Rectangle> result = new ArrayList<>();
+    return result;
   }
+
+    /**
+     * @param buildings: A list of lists of integers
+     * @return: Find the outline of those buildings
+     */
+    public List<List<Integer>> buildingOutline(int[][] buildings) {
+        // sort each critical points with x axis
+        // if they are in the same positions, put start point first
+        // to deal with [1, 2, 5] [2, 4, 5] situation
+        // entry point : {x, -height}
+        // exit point : {y, height}
+        List<int[]> points = new ArrayList<>();
+        for (int[] b : buildings) {
+            points.add(new int[] {b[0], -b[2]});
+            points.add(new int[] {b[1], b[2]});
+        }
+        Collections.sort(points, (a, b) -> {
+            if (a[0] == b[0]) return a[1] - b[1];
+            return a[0] - b[0];
+        });
+
+        // use TreeMap to keep track max height in current point
+        // height -> count
+        TreeMap<Integer, Integer> bst = new TreeMap<>(Collections.reverseOrder());
+        // ground, deal with empty tree case
+        bst.put(0, 1);
+
+        // for each critical point
+        int prevHeight = 0;
+        int prevIndex = 0;
+        List<List<Integer>> intervals = new ArrayList<>();
+        for (int[] p : points) {
+            // if entry point, increase that height count
+            if (p[1] < 0) {
+                bst.put(-p[1], bst.getOrDefault(-p[1], 0) + 1);
+            }
+            // if exit point, remove that height count
+            else {
+                bst.put(p[1], bst.getOrDefault(p[1], 0) - 1);
+                if (bst.get(p[1]) == 0) bst.remove(p[1]);
+            }
+            // root of tree is the current max height
+            int curHeight = bst.firstKey();
+            // if current max height != previous height
+            // merge the interval
+            if (curHeight != prevHeight) {
+                if ((prevIndex - p[0]) * prevHeight != 0) {
+                    intervals.add(getInterval(prevIndex, p[0], prevHeight));
+                }
+                prevIndex = p[0];
+                prevHeight = curHeight;
+
+            }
+        }
+        return intervals;
+    }
+
+    private List<Integer> getInterval(int s, int e, int h) {
+        return Arrays.asList(s, e, h);
+    }
 
   public static void main(String[] args) {
     System.exit(
