bc0d5eff303c66d4d670b58cc3ee7380d7cd8588
yusong-shen <yusongtju@gmail.com>
2018-11-03 22:30:59 -0500

    7.1 - Fail at last case Integer.MIN_VALUE

--- epi_judge_java/epi/StringIntegerInterconversion.java
+++ epi_judge_java/epi/StringIntegerInterconversion.java
@@ -5,12 +5,32 @@ import epi.test_framework.TestFailure;
 public class StringIntegerInterconversion {
 
   public static String intToString(int x) {
-    // TODO - you fill in here.
-    return "";
+    if (x == 0) return "0";
+    boolean isNegative = x < 0;
+    StringBuilder sb = new StringBuilder();
+    while (x != 0) {
+      sb.append(Math.abs(x) % 10);
+      x = x / 10;
+    }
+    if (isNegative) sb.append("-");
+    return sb.reverse().toString();
+//    return "" + x;
   }
   public static int stringToInt(String s) {
-    // TODO - you fill in here.
-    return 0;
+//    return Integer.parseInt(s);
+    int value = 0;
+    int sign = 1;
+    int i = 0;
+    if (s.charAt(i) == '+') {
+      i++;
+    } else if (s.charAt(i) == '-'){
+      sign = -1;
+      i++;
+    }
+    for (; i < s.length(); i++) {
+      value = value * 10 + (s.charAt(i) - '0');
+    }
+    return sign * value;
   }
   @EpiTest(testDataFile = "string_integer_interconversion.tsv")
   public static void wrapper(int x, String s) throws TestFailure {
