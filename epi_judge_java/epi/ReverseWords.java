8857c64ad2fa42a211783e749475644efd99f386
yusong-shen <yusongtju@gmail.com>
2018-11-04 19:22:04 -0600

    7.6 - reverse words, stuck at test 3

--- epi_judge_java/epi/ReverseWords.java
+++ epi_judge_java/epi/ReverseWords.java
@@ -5,9 +5,33 @@ import epi.test_framework.TimedExecutor;
 public class ReverseWords {
 
   public static void reverseWords(char[] input) {
-    // TODO - you fill in here.
+    String s = new String(input);
+    String[] words = s.split(" ");
+    reverse(words);
+    int k = 0;
+    for (int i = 0; i < words.length; i++) {
+      String w = words[i];
+      for (int j = 0; j < w.length(); j++) {
+        input[k++] = w.charAt(j);
+      }
+      if (i != words.length - 1) {
+        input[k++] = ' ';
+      }
+    }
     return;
   }
+
+  private static void reverse(String[] words) {
+    int l = 0, r = words.length - 1;
+    while (l < r) {
+      String temp = words[l];
+      words[l] = words[r];
+      words[r] = temp;
+      l++;
+      r--;
+    }
+  }
+
   @EpiTest(testDataFile = "reverse_words.tsv")
   public static String reverseWordsWrapper(TimedExecutor executor, String s)
       throws Exception {
