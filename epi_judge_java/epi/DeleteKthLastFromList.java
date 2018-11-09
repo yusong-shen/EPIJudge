19174c6d7ab31de2c5a24840db674aa2b3805ef0
yusong-shen <yusongtju@gmail.com>
2018-11-08 23:29:52 -0600

    7.7 - remove the kth last node (k start with 1)

--- epi_judge_java/epi/DeleteKthLastFromList.java
+++ epi_judge_java/epi/DeleteKthLastFromList.java
@@ -6,8 +6,19 @@ public class DeleteKthLastFromList {
 
   // Assumes L has at least k nodes, deletes the k-th last node in L.
   public static ListNode<Integer> removeKthLast(ListNode<Integer> L, int k) {
-    // TODO - you fill in here.
-    return null;
+    ListNode<Integer> dummyHead = new ListNode<>(-1, L);
+    ListNode<Integer> fast = dummyHead;
+    ListNode<Integer> slow = dummyHead;
+    for (int i = 0; i < k; i++) {
+      fast = fast.next;
+    }
+    while (fast.next != null) {
+      slow = slow.next;
+      fast = fast.next;
+    }
+    slow.next = slow.next.next;
+
+    return dummyHead.next;
   }
 
   public static void main(String[] args) {
