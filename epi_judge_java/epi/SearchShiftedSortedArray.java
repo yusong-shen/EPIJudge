ca4b96fc6a158cd044fb41ef1d0e1ee4b68d1978
yusong-shen <yusongtju@gmail.com>
2018-11-11 12:22:43 -0600

    12.3 - search shifted sorted array

--- epi_judge_java/epi/SearchShiftedSortedArray.java
+++ epi_judge_java/epi/SearchShiftedSortedArray.java
@@ -5,9 +5,20 @@ import java.util.List;
 public class SearchShiftedSortedArray {
   @EpiTest(testDataFile = "search_shifted_sorted_array.tsv")
 
+  // elements are all distinct
   public static int searchSmallest(List<Integer> A) {
-    // TODO - you fill in here.
-    return 0;
+    int l = 0, r = A.size() - 1;
+    while (l < r) {
+      int mid = l + (r - l) / 2;
+      // since mid is round down, we need to compare with A[r]
+      // think about a 2 element case like [3, 4] or [4, 3]
+      if (A.get(mid) > A.get(r)) {
+        l = mid + 1;
+      } else {
+        r = mid;
+      }
+    }
+    return r;
   }
 
   public static void main(String[] args) {
