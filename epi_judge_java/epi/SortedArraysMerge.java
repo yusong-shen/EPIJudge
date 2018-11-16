950db59d64227cf67626f243c53da3ce2844b4c4
yusong-shen <yusongtju@gmail.com>
2018-11-15 22:38:01 -0600

    10.1 heap - merge k sorted arrays

--- epi_judge_java/epi/SortedArraysMerge.java
+++ epi_judge_java/epi/SortedArraysMerge.java
@@ -1,14 +1,39 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class SortedArraysMerge {
   @EpiTest(testDataFile = "sorted_arrays_merge.tsv")
 
+
   public static List<Integer>
   mergeSortedArrays(List<List<Integer>> sortedArrays) {
-    // TODO - you fill in here.
-    return null;
+    int n = sortedArrays.size();
+    List<Integer> result = new ArrayList<>();
+    // (arrayId, headValue)
+    PriorityQueue<List<Integer>> minHeap = new PriorityQueue<>(n, Comparator.comparingInt(a -> a.get(1)));
+    List<Iterator<Integer>> iters = new ArrayList<>();
+    for (List<Integer> arr : sortedArrays) {
+      iters.add(arr.iterator());
+    }
+    // initialize
+    for (int i = 0; i < n; i++) {
+      if (iters.get(i).hasNext()) {
+        minHeap.add(Arrays.asList(i, iters.get(i).next()));
+      }
+    }
+    while (!minHeap.isEmpty()) {
+      List<Integer> top = minHeap.poll();
+      int iterId = top.get(0), headValue = top.get(1);
+      result.add(headValue);
+      if (iters.get(iterId).hasNext()) {
+        minHeap.add(Arrays.asList(iterId, iters.get(iterId).next()));
+      }
+    }
+    return result;
   }
 
   public static void main(String[] args) {
