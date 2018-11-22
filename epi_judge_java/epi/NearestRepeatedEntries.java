4825d9e0dd70ab3ac0b6f7f8381838c22559b1af
yusong-shen <yusongtju@gmail.com>
2018-11-22 17:08:22 -0600

    13.6 hash table - NearestRepeatedEntries

--- epi_judge_java/epi/NearestRepeatedEntries.java
+++ epi_judge_java/epi/NearestRepeatedEntries.java
@@ -1,13 +1,27 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+
 public class NearestRepeatedEntries {
   @EpiTest(testDataFile = "nearest_repeated_entries.tsv")
 
   public static int findNearestRepetition(List<String> paragraph) {
-    // TODO - you fill in here.
-    return 0;
+    int closestDist = Integer.MAX_VALUE;
+    Map<String, Integer> wordIndexMap = new HashMap<>();
+    int ind = 0;
+    for (String word : paragraph) {
+      if (wordIndexMap.containsKey(word)) {
+        int dist = ind - wordIndexMap.get(word);
+        closestDist = Math.min(closestDist, dist);
+      }
+      wordIndexMap.put(word, ind);
+      ind++;
+    }
+    return closestDist == Integer.MAX_VALUE ? -1 : closestDist;
   }
 
   public static void main(String[] args) {
