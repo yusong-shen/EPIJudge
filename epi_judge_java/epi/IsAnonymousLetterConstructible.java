b20caf406f40f050cecf8aaa5209a799bcf6d9b3
yusong-shen <yusongtju@gmail.com>
2018-11-22 16:16:43 -0600

    12.2 hash table - is_anonymous_letter_constructible

--- epi_judge_java/epi/IsAnonymousLetterConstructible.java
+++ epi_judge_java/epi/IsAnonymousLetterConstructible.java
@@ -1,12 +1,29 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class IsAnonymousLetterConstructible {
   @EpiTest(testDataFile = "is_anonymous_letter_constructible.tsv")
 
   public static boolean isLetterConstructibleFromMagazine(String letterText,
                                                           String magazineText) {
-    // TODO - you fill in here.
+    Map<Character, Integer> count = new HashMap<>();
+    for (int i = 0; i < letterText.length(); i++) {
+      char c = letterText.charAt(i);
+      count.put(c, count.getOrDefault(c, 0) + 1);
+    }
+    for (int i = 0; i < magazineText.length(); i++) {
+      char c = magazineText.charAt(i);
+      if (count.containsKey(c)) {
+        count.put(c, count.get(c) - 1);
+      }
+    }
+    for (char key : count.keySet()) {
+      if (count.get(key) > 0) return false;
+    }
     return true;
   }
 
