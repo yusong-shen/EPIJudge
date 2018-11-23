870bec00d2ffca48588c52cb71fb3ee13da43ee9
yusong-shen <yusongtju@gmail.com>
2018-11-23 17:13:33 -0600

    14.5 -  rebuildBSTFromPreorder

--- epi_judge_java/epi/BstFromPreorder.java
+++ epi_judge_java/epi/BstFromPreorder.java
@@ -3,12 +3,25 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 import java.util.List;
 public class BstFromPreorder {
-  @EpiTest(testDataFile = "bst_from_preorder.tsv")
+  private static int ind;
 
+  @EpiTest(testDataFile = "bst_from_preorder.tsv")
   public static BstNode<Integer>
   rebuildBSTFromPreorder(List<Integer> preorderSequence) {
-    // TODO - you fill in here.
-    return null;
+    if (preorderSequence.isEmpty()) return null;
+    ind = 0;
+
+    return rebuildBSTHelper(preorderSequence, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private static  BstNode<Integer> rebuildBSTHelper(List<Integer> preorderSequence, int minVal, int maxVal) {
+    if (ind >= preorderSequence.size()) return null;
+    BstNode<Integer> root = new BstNode<>(preorderSequence.get(ind));
+    if (root.data < minVal || root.data > maxVal) return null;
+    ind++;
+    root.left = rebuildBSTHelper(preorderSequence, minVal, root.data);
+    root.right = rebuildBSTHelper(preorderSequence, root.data, maxVal);
+    return root;
   }
 
   public static void main(String[] args) {
