03e3b6a1441a7489a0ef176bbab5340fedac1da6
yusong-shen <yusongtju@gmail.com>
2018-11-23 15:11:13 -0600

    14.1 - validate BST

--- epi_judge_java/epi/IsTreeABst.java
+++ epi_judge_java/epi/IsTreeABst.java
@@ -5,8 +5,13 @@ public class IsTreeABst {
   @EpiTest(testDataFile = "is_tree_a_bst.tsv")
 
   public static boolean isBinaryTreeBST(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return true;
+    return isBSTWithRange(tree, Integer.MIN_VALUE, Integer.MAX_VALUE);
+  }
+
+  private static boolean isBSTWithRange(BinaryTreeNode<Integer> tree, Integer minVal, Integer maxVal) {
+    if (tree == null) return true;
+    if (tree.data < minVal || tree.data > maxVal) return false;
+    return isBSTWithRange(tree.left, minVal, tree.data) && isBSTWithRange(tree.right, tree.data, maxVal);
   }
 
   public static void main(String[] args) {
