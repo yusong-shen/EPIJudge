30975f6e038e4346986c15ad413d58aad250fcfd
yusong-shen <yusongtju@gmail.com>
2018-11-23 15:50:29 -0600

    14.2 -  BST higher() (stuck at test case 9)

--- epi_judge_java/epi/SearchFirstGreaterValueInBst.java
+++ epi_judge_java/epi/SearchFirstGreaterValueInBst.java
@@ -3,11 +3,21 @@ import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class SearchFirstGreaterValueInBst {
 
+  static BstNode<Integer> candidate = new BstNode<>(-1);
+
   public static BstNode<Integer> findFirstGreaterThanK(BstNode<Integer> tree,
                                                        Integer k) {
-    // TODO - you fill in here.
-    return null;
+    if (tree == null) return candidate;
+    if (k >= tree.data) {
+      findFirstGreaterThanK(tree.right, k);
+    } else {
+      candidate = tree;
+      findFirstGreaterThanK(tree.left, k);
+    }
+    return candidate;
   }
+
+
   @EpiTest(testDataFile = "search_first_greater_value_in_bst.tsv")
   public static int findFirstGreaterThanKWrapper(BstNode<Integer> tree,
                                                  Integer k) {
