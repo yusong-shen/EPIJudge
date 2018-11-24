0edd1f046915761d1a59537d5bf819f3e55a35fe
yusong-shen <yusongtju@gmail.com>
2018-11-24 13:53:47 -0600

    16.2 DP - Edit Distance O(len1 * len2) time and space

--- epi_judge_java/epi/LevenshteinDistance.java
+++ epi_judge_java/epi/LevenshteinDistance.java
@@ -5,8 +5,27 @@ public class LevenshteinDistance {
   @EpiTest(testDataFile = "levenshtein_distance.tsv")
 
   public static int levenshteinDistance(String A, String B) {
-    // TODO - you fill in here.
-    return 0;
+    int len1= A.length(), len2 = B.length();
+    int[][] dp = new int[len1 + 1][len2 + 1];
+    // init
+    for (int i = 0; i <= len1; i++) {
+      dp[i][0] = i;
+    }
+    for (int j = 0; j <= len2; j++) {
+      dp[0][j] = j;
+    }
+    for (int i = 1; i <= len1; i++) {
+      for (int j = 1; j <= len2; j++) {
+        int substitute = (A.charAt(i - 1) == B.charAt(j - 1)) ?
+                dp[i - 1][j - 1] : dp[i - 1][j - 1] + 1;
+        int insert = dp[i][j - 1] + 1;
+        int delete = dp[i - 1][j] + 1;
+        dp[i][j] = Math.min(substitute, Math.min(insert, delete));
+      }
+    }
+
+
+    return dp[len1][len2];
   }
 
   public static void main(String[] args) {
