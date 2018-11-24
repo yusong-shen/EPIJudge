95440374a0b18396d73c8c2edf9b3fb7d79626bf
yusong-shen <yusongtju@gmail.com>
2018-11-24 13:29:40 -0600

    16.1 DP - O(s * n) time (s : score, n : number of plays) and O(s) space

--- epi_judge_java/epi/NumberOfScoreCombinations.java
+++ epi_judge_java/epi/NumberOfScoreCombinations.java
@@ -10,7 +10,7 @@ public class NumberOfScoreCombinations {
   @EpiTest(testDataFile = "number_of_score_combinations.tsv")
 
   public static int
-  numCombinationsForFinalScore(int finalScore,
+  numCombinationsForFinalScore2(int finalScore,
                                List<Integer> individualPlayScores) {
     if (individualPlayScores.isEmpty()) return 0;
     // dp[i][j] : # of ways to get score j, by using individualPlayScores.subList(0, i+1)
@@ -31,6 +31,29 @@ public class NumberOfScoreCombinations {
     return dp[individualPlayScores.size() - 1][finalScore];
   }
 
+    public static int
+    numCombinationsForFinalScore(int finalScore,
+                                 List<Integer> individualPlayScores) {
+        if (individualPlayScores.isEmpty()) return 0;
+        // dp[i][j] : # of ways to get score j, by using individualPlayScores.subList(0, i+1)
+        // by noticing the above equation only depends on dp[i - 1][j] and dp[i][j - w]
+        // we can use rolling array
+        int[][] dp = new int[2][finalScore + 1];
+        // initialization
+        for (int k = 0; k * individualPlayScores.get(0) <= finalScore; k++) {
+            dp[0][k * individualPlayScores.get(0)] = 1;
+        }
+        for (int i = 1; i < individualPlayScores.size(); i++) {
+            for (int j = 0; j < finalScore + 1; j++) {
+                int withoutCurrentPlay = dp[(i - 1) % 2][j];
+                int withCurrentPlay = j - individualPlayScores.get(i) >= 0 ?
+                        dp[i % 2][j - individualPlayScores.get(i)] : 0;
+                dp[i % 2][j] = withoutCurrentPlay + withCurrentPlay;
+            }
+        }
+
+        return dp[(individualPlayScores.size() - 1) % 2][finalScore];
+    }
 
   public static void main(String[] args) {
     System.exit(
