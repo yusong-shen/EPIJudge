878ad3e73255c0b6312f21dc7c5287eb00ed7ab9
yusong-shen <yusongtju@gmail.com>
2018-11-23 20:11:54 -0600

    14.8 (15.9) -  buildMinHeightBSTFromSortedArray

--- epi_judge_java/epi/BstFromSortedArray.java
+++ epi_judge_java/epi/BstFromSortedArray.java
@@ -2,7 +2,6 @@ package epi;
 import epi.test_framework.BinaryTreeUtils;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import epi.test_framework.TestFailure;
 import epi.test_framework.TestUtils;
 import epi.test_framework.TimedExecutor;
 import java.util.List;
@@ -10,9 +9,19 @@ public class BstFromSortedArray {
 
   public static BstNode<Integer>
   buildMinHeightBSTFromSortedArray(List<Integer> A) {
-    // TODO - you fill in here.
-    return null;
+
+    return buildMinHeightBSTFromSortedArrayHelper(A, 0, A.size() - 1);
   }
+
+  private static BstNode<Integer> buildMinHeightBSTFromSortedArrayHelper(List<Integer> A, int start, int end) {
+    if (start > end) return null;
+    int midIndex = start + (end - start) / 2;
+    BstNode<Integer> root = new BstNode<>(A.get(midIndex));
+    root.left = buildMinHeightBSTFromSortedArrayHelper(A, start, midIndex - 1);
+    root.right = buildMinHeightBSTFromSortedArrayHelper(A, midIndex + 1, end);
+    return root;
+  }
+
   @EpiTest(testDataFile = "bst_from_sorted_array.tsv")
   public static int
   buildMinHeightBSTFromSortedArrayWrapper(TimedExecutor executor,
