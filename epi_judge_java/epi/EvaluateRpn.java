7b4f2c58b043e228384556a7093b26943c5a15f5
yusong-shen <yusongtju@gmail.com>
2018-11-28 22:46:30 -0600

    8.2 - Eval RPN

--- epi_judge_java/epi/EvaluateRpn.java
+++ epi_judge_java/epi/EvaluateRpn.java
@@ -1,12 +1,46 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.LinkedList;
+
 public class EvaluateRpn {
   @EpiTest(testDataFile = "evaluate_rpn.tsv")
 
   public static int eval(String expression) {
-    // TODO - you fill in here.
-    return 0;
+    Deque<Integer> stack = new LinkedList<>();
+    String[] elements = expression.split(",");
+    for (String e : elements) {
+      if (Arrays.asList("+", "-", "*", "/").contains(e)) {
+        int right = stack.removeLast();
+        int left = stack.removeLast();
+        stack.addLast(expr(left, right, e));
+      } else {
+        stack.addLast(Integer.parseInt(e));
+      }
+    }
+    return stack.getLast();
+  }
+
+  private static int expr(int left, int right, String operators) {
+    int result = 0;
+    switch (operators) {
+      case "+":
+        result = left + right;
+        break;
+      case "-":
+        result = left - right;
+        break;
+      case "*":
+        result = left * right;
+        break;
+      case "/":
+        result = left / right;
+        break;
+    }
+    return result;
   }
 
   public static void main(String[] args) {
