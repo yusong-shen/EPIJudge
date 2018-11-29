fc733b1c13876db41cff07125de04905d5a767b7
yusong-shen <yusongtju@gmail.com>
2018-11-28 21:29:16 -0600

    8.6 - binary tree level order traversal

--- epi_judge_java/epi/TreeLevelOrder.java
+++ epi_judge_java/epi/TreeLevelOrder.java
@@ -1,14 +1,35 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
-import java.util.List;
+
+import java.util.*;
+
 public class TreeLevelOrder {
   @EpiTest(testDataFile = "tree_level_order.tsv")
 
   public static List<List<Integer>>
   binaryTreeDepthOrder(BinaryTreeNode<Integer> tree) {
-    // TODO - you fill in here.
-    return null;
+    if (tree == null) return Collections.emptyList();
+    Queue<BinaryTreeNode> q = new LinkedList<>();
+    List<List<Integer>> result = new ArrayList<>();
+    q.offer(tree);
+    while (!q.isEmpty()) {
+      int qsize = q.size();
+      List<Integer> level = new ArrayList<>();
+      for (int i = 0; i < qsize; i++) {
+        BinaryTreeNode<Integer> cur = q.poll();
+        level.add(cur.data);
+        if (cur.left != null) {
+          q.offer(cur.left);
+        }
+        if (cur.right != null) {
+          q.offer(cur.right);
+        }
+      }
+      result.add(level);
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
