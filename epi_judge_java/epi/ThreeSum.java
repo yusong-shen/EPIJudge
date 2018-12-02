83f9dfcde24ffb0982c84d9953d39d8beb950dc2
yusong-shen <yusongtju@gmail.com>
2018-12-02 15:46:00 -0600

    17.3 - 3 sum, sorting, can use entry multiple times

--- epi_judge_java/epi/ThreeSum.java
+++ epi_judge_java/epi/ThreeSum.java
@@ -1,14 +1,36 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Collections;
 import java.util.List;
 public class ThreeSum {
   @EpiTest(testDataFile = "three_sum.tsv")
 
   public static boolean hasThreeSum(List<Integer> A, int t) {
-    // TODO - you fill in here.
-    return true;
+    Collections.sort(A);
+    for (int num : A) {
+      if (hasTwoSum(A, t - num)) return true;
+    }
+
+    return false;
   }
+
+  public static boolean hasTwoSum(List<Integer> A, int t) {
+    int i = 0, j = A.size() - 1;
+    while (i <= j) {
+      int sum = A.get(i) + A.get(j);
+      if (sum == t) {
+        return true;
+      } else if (sum < t) {
+        i++;
+      } else {
+        j--;
+      }
+    }
+    return false;
+  }
+
 
   public static void main(String[] args) {
     System.exit(
