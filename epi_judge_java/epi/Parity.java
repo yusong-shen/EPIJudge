0f8d37f82b01f789b0566d5970c561ed02e898a8
yusong-shen <yusongtju@gmail.com>
2018-12-02 09:14:55 -0600

    4.1 - bit parity , use (x & (x - 1)) to clear the lowest set bit

--- epi_judge_java/epi/Parity.java
+++ epi_judge_java/epi/Parity.java
@@ -4,8 +4,13 @@ import epi.test_framework.GenericTest;
 public class Parity {
   @EpiTest(testDataFile = "parity.tsv")
   public static short parity(long x) {
-    // TODO - you fill in here.
-    return 0;
+    short result = 0;
+    while (x != 0) {
+      result ^= 1;
+      x = x & (x - 1);
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
