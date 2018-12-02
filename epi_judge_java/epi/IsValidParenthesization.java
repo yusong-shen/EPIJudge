41946e548b1618b6415d331b8d3c8232fbb76d77
yusong-shen <yusongtju@gmail.com>
2018-12-02 08:51:11 -0600

    8.3 - valid parenthesis

--- epi_judge_java/epi/IsValidParenthesization.java
+++ epi_judge_java/epi/IsValidParenthesization.java
@@ -1,12 +1,33 @@
 package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
 public class IsValidParenthesization {
   @EpiTest(testDataFile = "is_valid_parenthesization.tsv")
 
   public static boolean isWellFormed(String s) {
-    // TODO - you fill in here.
-    return true;
+    Map<Character, Character> parenthesisMap = new HashMap<>();
+    parenthesisMap.put(')', '(');
+    parenthesisMap.put(']', '[');
+    parenthesisMap.put('}', '{');
+    Deque<Character> stack = new LinkedList<>();
+    for (int i = 0; i < s.length(); i++) {
+      char ch = s.charAt(i);
+      if (parenthesisMap.values().contains(ch)) {
+        stack.addLast(ch);
+      } else if (parenthesisMap.containsKey(ch)){
+        if (stack.isEmpty()) return false;
+        if (!parenthesisMap.get(ch).equals(stack.getLast())) return false;
+        stack.removeLast();
+      }
+    }
+
+    return stack.isEmpty();
   }
 
   public static void main(String[] args) {
