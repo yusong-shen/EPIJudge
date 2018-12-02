ea439d2166ea7e9c920aaa57c2c3175a52f89545
yusong-shen <yusongtju@gmail.com>
2018-12-02 10:58:14 -0600

    4.3 - reverse bits by precompute 16 bits

--- epi_judge_java/epi/ReverseBits.java
+++ epi_judge_java/epi/ReverseBits.java
@@ -2,10 +2,28 @@ package epi;
 import epi.test_framework.EpiTest;
 import epi.test_framework.GenericTest;
 public class ReverseBits {
+
+  static long[] precompute = new long[1<<16];
+
+  static {
+    for (long x = 0; x < precompute.length; x++) {
+      precompute[(int) x] = reverseBits(x, 15);
+    }
+  }
+
   @EpiTest(testDataFile = "reverse_bits.tsv")
   public static long reverseBits(long x) {
-    for (int i = 0; i < 32; i++) {
-      x = swapBits(x, i, 63 - i);
+    int WORD_SIZE = 16;
+    int BIT_MASK = 0xFFFF;
+    return precompute[(int) (x >>> (WORD_SIZE * 3) & BIT_MASK)]
+            | precompute[(int) (x >>> (WORD_SIZE * 2) & BIT_MASK)] << WORD_SIZE
+            | precompute[(int) (x >>> WORD_SIZE & BIT_MASK)] << (WORD_SIZE * 2)
+            | precompute[(int) (x & BIT_MASK)] << (WORD_SIZE * 3);
+  }
+
+  public static long reverseBits(long x, int n) {
+    for (int i = 0, j = n; i < j; i++, j--) {
+      x = swapBits(x, i, j);
     }
     return x;
   }
@@ -19,6 +37,8 @@ public class ReverseBits {
   }
 
   public static void main(String[] args) {
+
+
     System.exit(
         GenericTest
             .runFromAnnotations(args, "ReverseBits.java",
