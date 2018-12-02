0993596d8376dc1d5146ec701054adbea68d090d
yusong-shen <yusongtju@gmail.com>
2018-12-02 10:31:47 -0600

    4.2 - swap bits

--- epi_judge_java/epi/SwapBits.java
+++ epi_judge_java/epi/SwapBits.java
@@ -4,8 +4,11 @@ import epi.test_framework.GenericTest;
 public class SwapBits {
   @EpiTest(testDataFile = "swap_bits.tsv")
   public static long swapBits(long x, int i, int j) {
-    // TODO - you fill in here.
-    return 0;
+    if (((x >>> i) & 1) != ((x >>> j) & 1)) {
+      long mask = (1l << i) | (1l << j);
+      x ^= mask;
+    }
+    return x;
   }
 
   public static void main(String[] args) {
