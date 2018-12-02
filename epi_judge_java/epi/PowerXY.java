7775581016a2b211e8451f5916de22dfe6aba411
yusong-shen <yusongtju@gmail.com>
2018-12-02 11:46:18 -0600

    4.i - pow(x, y)

--- epi_judge_java/epi/PowerXY.java
+++ epi_judge_java/epi/PowerXY.java
@@ -4,8 +4,27 @@ import epi.test_framework.GenericTest;
 public class PowerXY {
   @EpiTest(testDataFile = "power_x_y.tsv")
   public static double power(double x, int y) {
-    // TODO - you fill in here.
-    return 0.0;
+    double result = 1.0;
+    if (y < 0) {
+      y = -y;
+      x = 1.0 / x;
+    }
+    // y is even number
+    // x^y = (x^(y/2))^2
+    // y is odd number
+    // x^y = x * (x^(y/2))^2
+    while (y != 0) {
+      // y is odd
+      if ((y & 1) != 0) {
+        result *= x;
+      }
+      // x = x ^ 2
+      x *= x;
+      // y = y / 2
+      y = y >>> 1;
+    }
+
+    return result;
   }
 
   public static void main(String[] args) {
