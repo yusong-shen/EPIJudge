b8c5c5b8c687a6350672d8abc7f6b74ee0a1f390
yusong-shen <yusongtju@gmail.com>
2018-12-10 14:19:34 -0600

    dp 16.3 (17.3) - Count the number of ways to traverse a 2D array

--- epi_judge_java/epi/NumberOfTraversalsMatrix.java
+++ epi_judge_java/epi/NumberOfTraversalsMatrix.java
@@ -5,8 +5,21 @@ public class NumberOfTraversalsMatrix {
   @EpiTest(testDataFile = "number_of_traversals_matrix.tsv")
 
   public static int numberOfWays(int n, int m) {
-    // TODO - you fill in here.
-    return 0;
+    int[][] dp = new int[n][m];
+    // init
+    for (int i = 0; i < n; i++) {
+      dp[i][0] = 1;
+    }
+    for (int j = 1; j < m; j++) {
+      dp[0][j] = 1;
+    }
+    for (int i = 1; i < n; i++) {
+      for (int j = 1; j < m; j++) {
+        dp[i][j] = dp[i - 1][j] + dp[i][j - 1];
+      }
+    }
+
+    return dp[n - 1][m - 1];
   }
 
   public static void main(String[] args) {
